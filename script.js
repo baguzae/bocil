50d9c84954076a16fc45b3a3bffd3d716076d200
opet <opet@gmail.com>
2024-11-21 22:54:13 +0700

    Update proyek dengan perubahan pada home.html

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function playMusicAndRedirect() {
-  const audio = document.getElementById('birthday-audio');
-  audio.play(); // Mulai memutar musik
-  setTimeout(() => {
-      window.location.href = "home.html"; // Redirect setelah musik dimulai
-  }, 500); // Delay sedikit agar musik mulai sebelum redirect
-}
+// function playMusicAndRedirect() {
+//   const audio = document.getElementById('birthday-audio');
+//   audio.play(); // Mulai memutar musik
+//   setTimeout(() => {
+//       window.location.href = "home.html"; // Redirect setelah musik dimulai
+//   }, 500); // Delay sedikit agar musik mulai sebelum redirect
+// }
+
+const audio = document.getElementById('audioPlayer');
+        audio.onplay = () => alert("Audio is playing");
+        audio.onerror = () => alert("Audio failed to load");
